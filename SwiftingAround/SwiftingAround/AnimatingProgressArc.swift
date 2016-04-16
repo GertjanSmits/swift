d7be6bbafb77d9a9ffacb91b1f85fdd8214681d3
Gertjan Smits <gertjan@imgzine.com>
2016-04-16 20:57:52 +0200

    setting up ViewControllers

---
+++ SwiftingAround/SwiftingAround/AnimatingProgressArc.swift
@@ -0,0 +1,25 @@
+//
+//  AnimatingProgressArc.swift
+//  SwiftingAround
+//
+//  Created by Gertjan Smits on 16-04-16.
+//  Copyright © 2016 Gertjan.com. All rights reserved.
+//
+
+import UIKit
+
+class AnimatingProgressArc:UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.view.backgroundColor = UIColor.redColor()
+	}
+}
+
+class SomethingBlue:UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.view.backgroundColor = UIColor.blueColor()
+	}
+}
